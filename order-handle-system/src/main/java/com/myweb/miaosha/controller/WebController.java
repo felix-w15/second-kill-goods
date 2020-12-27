10200c63b7a60dd7bb5c34424f8ca50d8c00c358
VanishingCake <806547150@qq.com>
2020-12-27 15:07:39 +0800

    实现抢购界面与数据库服务

--- order-handle-system/src/main/java/com/myweb/miaosha/controller/WebController.java
+++ order-handle-system/src/main/java/com/myweb/miaosha/controller/WebController.java
@@ -1,8 +1,30 @@
 package com.myweb.miaosha.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.myweb.miaosha.entity.Customer;
+import com.myweb.miaosha.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+import javax.servlet.http.HttpSession;
+import java.util.Map;
+
+@Controller
 public class WebController {
+    @Autowired
+    CustomerService customerService;
 
+    @GetMapping(value = "/login")
+    public String login(){
+        return "login";
+    }
+    @PostMapping(value = "/doLogin")
+    @ResponseBody
+    public String doLogin(@RequestParam("username") String username, @RequestParam("password") String password,
+                          HttpSession session, Model model){
+        Customer customer = customerService.getCustomer(username);
+
+        return "";
+    }
 }
